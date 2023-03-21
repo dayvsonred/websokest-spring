2a147f98fbff664995d212b2dd0a235244f435a5
dayvson_menezes_local <dayvison.assis@gmail.com>
2023-03-20 22:53:39 -0300

    start ok

---
+++ src/main/java/br/com/dasa/neph/socket/nephsocket/NephSocketApplication.java
@@ -0,0 +1,13 @@
+package br.com.dasa.neph.socket.nephsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NephSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NephSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dasa/neph/socket/nephsocket/NephSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dasa.neph.socket.nephsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NephSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
